3f494b8dfa296e101d22b0fde5673176b80b3ed1
l-kimleng <l.kimleng.surface.kh@hotmail.com>
2018-07-27 20:28:47 -0400

    Add project files.

---
+++ Zoolab/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Zoolab.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Zoolab/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Zoolab.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
